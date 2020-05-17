84f23bab7ea730805706295f02f80843bf85cc68
Heber Ribeiro <heber.r@outlook.com>
2020-05-17 13:08:41 -0300

    Input Range change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 window.addEventListener('load', start);
 
 function start() {
-  console.log('Página totalmente carregada');
+  var redRange = document.querySelector('#red-range');
+  var greenRange = document.querySelector('#green-range');
+  var blueRange = document.querySelector('#blue-range');
+
+  var redText = document.querySelector('#red-text');
+  var greenText = document.querySelector('#green-text');
+  var blueText = document.querySelector('#blue-text');
+
+  redRange.addEventListener('input', rangeChange);
+  greenRange.addEventListener('input', rangeChange);
+  blueRange.addEventListener('input', rangeChange);
+
+  function rangeChange(event) {
+    /**
+     * Identifies which input range has changed by capturing the element id,
+     * from there it assigns the value of the range to the input text.
+     */
+    switch (event.target.id) {
+      case 'red-range':
+        var redRangeValue = event.target.value;
+        redText.value = redRangeValue;
+        break;
+      case 'green-range':
+        var greenRangeValue = event.target.value;
+        greenText.value = greenRangeValue;
+        break;
+      case 'blue-range':
+        var blueRangeValue = event.target.value;
+        blueText.value = blueRangeValue;
+        break;
+      default:
+        break;
+    }
+  }
 }
